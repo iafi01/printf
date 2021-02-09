d4e2d6f7fbc7715799694f477a7409b54d82d3aa
IAFI <67833440+iafi01@users.noreply.github.com>
2021-02-09 15:47:21 +0100

    Add files via upload

--- ft_conversion_pointer.c
+++ ft_conversion_pointer.c
@@ -6,13 +6,24 @@
 /*   By: liafigli <liafigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 13:39:11 by liafigli          #+#    #+#             */
-/*   Updated: 2021/02/05 14:06:07 by liafigli         ###   ########.fr       */
+/*   Updated: 2021/02/09 14:57:00 by liafigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_conversion_pointer(int c, t_flags flags)
+int ft_conversion_pointer(unsigned long long c, t_flags flags)
 {
-    
+    char *pointer;
+    char count;
+    int len;
+
+    pointer = ft_baseall(c, 16, 0);
+    len = ft_strlen(pointer);
+    if (flags.minus == 1)
+        ft_putstr(pointer);
+    count = ft_width(flags.width, len, 0);
+    if (flags.minus == 0)
+        ft_putstr(pointer);
+    return (count + len);
 }
