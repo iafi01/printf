4062708c80e2703b68c233155914e1310137ec2d
IAFI <67833440+iafi01@users.noreply.github.com>
2021-02-16 16:38:08 +0100

    Add files via upload

--- ft_conversion_integer.c
+++ ft_conversion_integer.c
@@ -6,7 +6,7 @@
 /*   By: liafigli <liafigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 13:39:11 by liafigli          #+#    #+#             */
-/*   Updated: 2021/02/15 18:02:30 by liafigli         ###   ########.fr       */
+/*   Updated: 2021/02/16 16:36:57 by liafigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,69 +17,72 @@ int ft_conversion_integer(int c, t_flags flags)
     int count;
     char *str;
     int len;
-
+    int neg;
+    int flag;
+    
+    flag = 0;
+    neg = 0;
+    count = 0;
+    if (c < 0)
+    {
+        c *= -1;
+        neg = -1;
+    }
     str = ft_itoa(c);
     len = ft_strlen(str);
-    count = 0;
+    if (flags.precision == 0 && c == 0)
+    {
+        str = ft_strdup("");
+        count = ft_width(flags.width, 0, flags.zero);
+        return (count);
+    }
     if (flags.minus == 1)
     {
+        if (neg < 0 && flag == 0)
+        {
+            ft_putchar('-');
+            flag = 1;
+        }
+        count += ft_width(flags.precision, len, 1);
         ft_putstr(str);
-        ft_width(flags.precision, len, 1);
     }
-    count = ft_width(flags.width, len, 0);
+    if (flags.zero > 0)
+    {
+ /*       if (flags.width > flags.precision)
+           count = ft_width(flags.width, flags.precision, 0);*/
+        if (flags.precision < len && flags.precision > 0)
+            flags.zero = 0;
+    }
+    if (flags.precision > len)
+    {
+        if (flags.width <= flags.precision)
+        {
+            if (neg < 0 && flag == 0)
+            {
+                ft_putchar('-');
+                flag = 1;
+            }
+        }
+        count = ft_width(flags.width + neg, flags.precision, flags.width <= flags.precision );  
+    }
+    else if (flags.precision <= len)
+    {
+        if (flags.zero)
+        {
+            if (neg < 0 && flag == 0)
+            {
+                ft_putchar('-');
+                flag = 1;
+            }
+        }
+        count = ft_width(flags.width + neg, len + count, flags.zero);
+    }
     if (flags.minus == 0)
     {
+        if (neg < 0 && flag == 0)
+            ft_putchar('-');
         ft_width(flags.precision, len, 1);
         ft_putstr(str);
     }
     return (count + len);
 }
-
-/*
-int ft_putzero(int c)
-{
-    int i;
-    char *str;
-
-    str =NULL;
-    i = 0;
-    while (c /= 10)
-    {
-        str[i] = '0';
-        i++;
-    }
-    ft_putstr(str);
-    return (i);
-}
-
-int ft_conversion_integer(int c, t_flags flags)
-{
-    int count;
-    char *str;
-    int neg;
-
-    if (c < 0)
-    {
-        neg = -1;
-        c *= -1;
-    }
-    str = ft_itoa(c);
-    count = 0;
-    if (flags.minus == 1)
-    {
-        if (neg == -1)
-            count += ft_putchar('-');
-        count += ft_putzero(c);
-        count += ft_putstr(str);
-    }
-    count = ft_width(flags.width, ft_strlen(str), 0);
-    if (flags.minus == 0)
-    {
-        if (neg == -1)
-            count += ft_putchar('-');
-        count += ft_putzero(c);
-        count += ft_putstr(str);
-    }
-    return (count);
-}
-*/
