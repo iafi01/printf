37947f288fe21dd147b42386d2caf36d708d309a
IAFI <67833440+iafi01@users.noreply.github.com>
2021-02-17 17:11:39 +0100

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -6,92 +6,91 @@
 /*   By: liafigli <liafigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 13:43:00 by liafigli          #+#    #+#             */
-/*   Updated: 2021/02/17 14:40:43 by liafigli         ###   ########.fr       */
+/*   Updated: 2021/02/17 17:10:38 by liafigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-t_flags ft_init_flags(void)
+t_flags	ft_init_flags(void)
 {
-    t_flags flags;
+	t_flags flags;
 
-    flags.width = 0;
-    flags.type = 0;
-    flags.precision = -1;
-    flags.zero = 0;
-    flags.star = 0;
-    flags.minus = 0;
-    return (flags);
+	flags.width = 0;
+	flags.type = 0;
+	flags.precision = -1;
+	flags.zero = 0;
+	flags.star = 0;
+	flags.minus = 0;
+	return (flags);
 }
 
-int ft_check_flags(const char *s, int i, t_flags *flags, va_list args)
+int		ft_check_flags(const char *s, int i, t_flags *flags, va_list args)
 {
-    while (s[i] != '.' && !ft_is_type(s[i]))
-    {
-        if (!ft_isdigit(s[i]) && !ft_is_type(s[i]) && !ft_is_flags(s[i]))
-            break;
-        if (s[i] == '0' && flags->width == 0 && flags->minus == 0)
-            flags->zero = 1;
-        if (s[i] == '-')
-            *flags = ft_flag_minus(*flags);
-        if (s[i] == '*')
-        {
-            flags->width = va_arg(args, int);
-            if (flags->width < 0)
-            {
-                flags->width *= -1;
-                *flags = ft_flag_minus(*flags);
-            }
-        }
-        if (ft_isdigit(s[i]))
-            flags->width = (flags->width * 10) + (s[i] - '0');
-        i++;
-    }
-    if (s[i] == '.')
-        i = ft_flag_dot(s, ++i, flags, args);
-    if (ft_is_type(s[i]))
-        flags->type = s[i];
-    return (i);
+	while (s[i] != '.' && !ft_is_type(s[i]))
+	{
+		if (s[i] == '0' && flags->width == 0 && flags->minus == 0)
+			flags->zero = 1;
+		if (s[i] == '-')
+			*flags = ft_flag_minus(*flags);
+		if (s[i] == '*')
+		{
+			flags->width = va_arg(args, int);
+			if (flags->width < 0)
+			{
+				flags->width *= -1;
+				*flags = ft_flag_minus(*flags);
+			}
+		}
+		if (ft_isdigit(s[i]))
+			flags->width = (flags->width * 10) + (s[i] - '0');
+		i++;
+	}
+	if (s[i] == '.')
+		i = ft_flag_dot(s, ++i, flags, args);
+	if (ft_is_type(s[i]))
+		flags->type = s[i];
+	return (i);
 }
 
-int ft_check(char *s, va_list args)
+int		ft_check(char *s, va_list args)
 {
-    int i;
-    t_flags flags;
-    int num;
+	int		i;
+	t_flags	flags;
+	int		num;
 
-    num = 0;
-    i = 0;
-    while (s[i])
-    {
-        flags = ft_init_flags();
-        if (s[i] == '%' && s[i + 1])
-        {
-            i = ft_check_flags(s, ++i, &flags, args);
-            if (ft_is_type(s[i]))
-                num += ft_conversion(s[i], flags, args);
-            else if (s[i])
-                num += ft_putchar(s[i]);
-        }
-        else if (s[i] != '%')
-            num += ft_putchar(s[i]);
-        i++;
-    }
-    return (num);
+	num = 0;
+	i = 0;
+	while (s[i])
+	{
+		flags = ft_init_flags();
+		if (s[i] == '%' && s[i + 1])
+		{
+			i = ft_check_flags(s, ++i, &flags, args);
+			if (ft_is_type(s[i]))
+				num += ft_conversion(s[i], flags, args);
+			else if (s[i])
+				num += ft_putchar(s[i]);
+		}
+		else if (s[i] != '%')
+			num += ft_putchar(s[i]);
+		i++;
+	}
+	return (num);
 }
 
 
-int ft_printf(const char *str, ...)
+int		ft_printf(const char *str, ...)
 {
-    char *s;
-    int num;
-    va_list args;
-    num = 0;
-    s = ft_strdup(str);
-    va_start(args, str);
-    num += ft_check(s, args);
-    va_end(args);
-    free(s);
-    return (num);
+	char	*s;
+	int		num;
+	va_list	args;
+
+	num = 0;
+	s = ft_strdup(str);
+	va_start(args, str);
+	num += ft_check(s, args);
+	va_end(args);
+	free(s);
+	return (num);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,10 +6,9 @@
 /*   By: liafigli <liafigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/24 16:46:07 by liafigli          #+#    #+#             */
-/*   Updated: 2021/02/17 12:03:06 by liafigli         ###   ########.fr       */
+/*   Updated: 2021/02/17 17:09:06 by liafigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
@@ -17,45 +16,47 @@
 # include "libft/libft.h"
 # include <stdarg.h>
 
-typedef struct s_flags
+typedef struct	s_flags
 {
-    int width;
-    int precision;
-    int type;
-    int zero;
-    int dot;
-    int star;
-    int minus;
-} t_flags;
+	int			width;
+	int			precision;
+	int			type;
+	int			zero;
+	int			dot;
+	int			star;
+	int			minus;
+}				t_flags;
 
-typedef struct s_int
+typedef struct	s_int
 {
-    int count;
-    int len;
-    int minus;
-    int neg;
-    int flag;
-} t_int;
+	int			count;
+	int			len;
+	int			minus;
+	int			neg;
+	int			flag;
+}				t_int;
 
-t_int   ft_init_int(void);
-char    *ft_lowercase(char *s);
-char	*ft_itoa_u(unsigned int n);
-int     ft_printf(const char *fix, ...);
-int     ft_is_flags(int c);
-int     ft_is_type(int c);
-t_flags ft_flag_minus(t_flags flags);
-int     ft_flag_dot(const char *s, int i, t_flags *flags, va_list args);
-int     ft_check_flags(const char *s, int i, t_flags *flags, va_list args);
-int     ft_check(char *s, va_list args);
-char    *ft_baseall(unsigned long long n, int base, int up);
-int     ft_width(int width, int leng, int zero);
-char	*ft_u_itoa(unsigned int n);
-int     ft_conversion(int c, t_flags flags, va_list args);
-int     ft_conversion_char(int c, t_flags flags);
-int     ft_conversion_string(char *str, t_flags flags);
-int     ft_conversion_pointer(unsigned long long c, t_flags flags);
-int     ft_conversion_hexa(unsigned int c, int cap, t_flags flags);
-int     ft_conversion_integer(int c, t_flags flags);
-int     ft_conversion_unsigned(unsigned int c, t_flags flags);
-int     ft_conversion_percent(t_flags flags);
+t_int			ft_init_int(void);
+char			*ft_lowercase(char *s);
+char			*ft_itoa_u(unsigned int n);
+int				ft_printf(const char *fix, ...);
+int				ft_is_flags(int c);
+int				ft_is_type(int c);
+t_flags			ft_flag_minus(t_flags flags);
+int				ft_flag_dot(const char *s, int i, t_flags *flags, va_list args);
+int				ft_check_flags(const char *s, int i, t_flags *flags,
+					va_list args);
+int				ft_check(char *s, va_list args);
+char			*ft_baseall(unsigned long long n, int base, int up);
+int				ft_width(int width, int leng, int zero);
+char			*ft_u_itoa(unsigned int n);
+int				ft_conversion(int c, t_flags flags, va_list args);
+int				ft_conversion_char(int c, t_flags flags);
+int				ft_conversion_string(char *str, t_flags flags);
+int				ft_conversion_pointer(unsigned long long c, t_flags flags);
+int				ft_conversion_hexa(unsigned int c, int cap, t_flags flags);
+int				ft_conversion_integer(int c, t_flags flags);
+int				ft_conversion_unsigned(unsigned int c, t_flags flags);
+int				ft_conversion_percent(t_flags flags);
+
 #endif
