6aeef9dcced2672d50aa4dffd6c59874ac2d53f6
IAFI <67833440+iafi01@users.noreply.github.com>
2021-02-19 12:03:39 +0100

    Add files via upload

--- ft_conversion_integer.c
+++ ft_conversion_integer.c
@@ -6,7 +6,7 @@
 /*   By: liafigli <liafigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/05 13:39:11 by liafigli          #+#    #+#             */
-/*   Updated: 2021/02/18 19:09:34 by liafigli         ###   ########.fr       */
+/*   Updated: 2021/02/19 10:21:54 by liafigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ t_int		ft_init_int(void)
 	i.flag = 0;
 	i.count = 0;
 	i.len = 0;
+	i.cn = 0;
 	return (i);
 }
 
@@ -89,9 +90,7 @@ int			ft_conversion_integer(int c, t_flags flags)
 {
 	t_int	i;
 	char	*str;
-	int 	cn;
 
-	cn = 0;
 	i = ft_init_int();
 	if (c == -2147483648)
 		i.flag = 1;
@@ -107,11 +106,11 @@ int			ft_conversion_integer(int c, t_flags flags)
 		str = ft_strdup("");
 		return (ft_width(flags.width, 0, flags.zero));
 	}
-	cn += ft_first(flags, &i, str);
+	i.cn += ft_first(flags, &i, str);
 	if (flags.zero > 0)
 		if (flags.precision <= i.len && flags.precision > 0)
 			flags.zero = 0;
-	cn += ft_second(flags, &i);
-	cn += ft_third(flags, &i, str);
-	return (i.count + cn);
+	i.cn += ft_second(flags, &i);
+	i.cn += ft_third(flags, &i, str);
+	return (i.count + i.cn);
 }
